052b8742b0189f9505e3c26e62d355528153d2b3
Kaushik Bhat <kaushik.bhat11@learner.manipal.edu>
2020-06-12 18:02:16 +0530

    Particles js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Particles from 'react-particles-js';
+//import './App.css';
+import './Test.css';
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+	return (
+		<div className="App">
+			<div className="home">
+				<Particles
+					params={{
+						"particles": {
+							"line_linked": {
+								"color": "#FFFFFF"
+							},
+							"number": {
+								"value": 70
+							},
+							"size": {
+								"value": 2
+							}
+						},
+						"interactivity": {
+							"events": {
+								"onhover": {
+									"enable": true,
+									"mode": "repulse"
+								}
+							}
+						}
+					}}
+				/>
+			</div>
+			<div className="title">
+				<p className="text-green">Hi, I am</p>
+				<h1>Kaushik <span className="text-green">Bhat</span></h1>
+			</div>
+			<div className="social-parent">
+				<div className="social-links">
+					<div class="social-container">
+						<ul class="social-icons">
+							<li><a href="#"><i class="fa fa-instagram"></i></a></li>
+							<li><a href="#"><i class="fa fa-github"></i></a></li>
+							<li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+							<li><a href="#"><i class="fa fa-briefcase"></i></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
